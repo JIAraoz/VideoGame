ee7ecac393f22f7cbdc6fc0b7e41f0a60b6b27e5
JIAraoz <ignacioaraoz18@gmai.com>
2024-09-30 04:23:08 -0300

    feat-characterClass

---
+++ src/Character.java
@@ -0,0 +1,29 @@
+public class Character {
+    private String name;
+    private int health;
+    private int totalCharacters;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getTotalCharacters() {
+        return totalCharacters;
+    }
+
+    public void setTotalCharacters(int totalCharacters) {
+        this.totalCharacters = totalCharacters;
+    }
+}
